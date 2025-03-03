f6f65812e9b974927fceb61dbb19b9fd93fe05e9
Delasi-Richards <delasi.richards@gmail.com>
2025-03-03 14:22:16 +0000

    2025.03.03:14.22 - Update

---
+++ readme.md
@@ -0,0 +1,1 @@
+This project aims to create a simple rewriter app in Python using spaCy models and Wordnet's synonyms list.
--- src/main.py
+++ src/main.py
@@ -9,16 +9,43 @@ The tests are: synonym similarity testing, and part of speech testing.
 """
 
 import spacy
+from nltk.corpus import wordnet
+
+
+def get_synonyms(term):
+	synonyms = set()  # Sets use less memory, and order is not important
+	for syn in wordnet.synsets(term):
+		for lemma in syn.lemmas():
+			synonyms.add(lemma.name().replace("_", " "))
+	return synonyms
 
 
 def rewrite(sentence: str):
-	nlp = spacy.load("en_core_web_sm")
-	doc = nlp(sentence)
-	for i in range(0, len(doc)):
+	nlp = spacy.load("en_core_web_lg")
+	sentence_doc = nlp(sentence)
+	for i in range(0, len(sentence_doc)):  # Parse every word in the sentence
 		# print(f"Name: {doc[i].text} \t Part of Speech: {doc[i].pos_} \t Lemma: {doc[i].lemma_}")
 
-		if doc[i].pos_ == "NOUN" or doc[i].pos_ == "VERB" or doc[i].pos_ == "ADJ":
-			print("pass")
+		# Filter out words whose synonyms are to be generated
+		if sentence_doc[i].pos_ == "NOUN" or sentence_doc[i].pos_ == "VERB" or sentence_doc[i].pos_ == "ADJ":
+
+			synonyms = [j for j in get_synonyms(sentence_doc[i].text)]  # Generate synonyms into a list variable
+			for j in range(0, len(synonyms)):  # This takes each synonym from the list of synonyms
+
+				# This block of code creates a sentence with the current word swapped with a synonym
+				syn_sentence_list = sentence_doc.text.split()
+				syn_sentence_list[i] = synonyms[j]  # This is the list variable which holds the sentence with the synonym inserted
+
+				# This block of code converts the list variable to a sentence variable
+				syn_sentence = ""
+				for k in syn_sentence_list:
+					syn_sentence += (k + " ")
+
+				# Check the similarity between the original sentence and the new sentence
+				syn_sentence = nlp(syn_sentence)
+				print(syn_sentence.text)
+				print(sentence_doc.similarity(syn_sentence))
 
 
-rewrite("The boy is washing the car")
+rewrite("The boy is here.")
+# print(get_synonyms("car"))
